acb51cbae09ce05218bd28e96787d5458be26220
kotteletfisk <ostekugle@gmail.com>
2024-04-29 10:33:25 +0200

    Updated extra space values. Added angles

--- SmartCubes/src/main/java/org/abstractica/smartcubes/base/plates/Plates.java
+++ SmartCubes/src/main/java/org/abstractica/smartcubes/base/plates/Plates.java
@@ -298,12 +298,12 @@ public class Plates
 		return csg.union3D(plate);
 	}
 
-	public Geometry3D scalableTile(int nHolesWidth, int nHolesHeight, boolean roundHoles, boolean extraWidth, boolean extraHeight)
+	public Geometry3D scalableTile(int nHolesWidth, int nHolesHeight, boolean roundHoles, int extraWidth, int extraHeight)
 	{
 		if (nHolesWidth < 2 || nHolesHeight < 2) throw new IllegalArgumentException("Tile must be at least 2x2");
 
-		double extraL = extraWidth ? scale*4 *2: 0;
-		double extraH = extraHeight ? scale*4 *2: 0;
+		double extraH = scale *4 *2 * extraHeight;
+		double extraL = scale *4 *2 * extraWidth;
 
 		double x = nHolesWidth * 16 + 8;
 		double y = nHolesHeight * 16 + 8;
@@ -340,6 +340,24 @@ public class Plates
 		return csg.cache(total);
 	}
 
+	public Geometry3D angleTile(int nHolesWidth, int nHolesHeight, int nHolesOffsetY, int extraOffsetY)
+	{
+		double x = nHolesWidth * 16 + 8;
+		double y = nHolesHeight * 16 + 8;
+		double offsetY = nHolesOffsetY * 16 + 8 + (scale * 4 * extraOffsetY);
+
+		double width = scale * x;
+		double height = scale * y;
+		double offset = scale * offsetY;
+
+		Geometry3D plate = csg.box3D(width, height, scale*4, false);
+		plate = csg.translate3D(width/2, height/2, 0).transform(plate);
+
+		Geometry3D angle = csg.rotate3DX(csg.degrees(-45)).transform(plate);
+		angle = csg.translate3DY(offset).transform(angle);
+		return angle;
+	}
+
 
 	public static void main(String[] args)
 	{
@@ -348,8 +366,11 @@ public class Plates
 		// Geometry3D tile = sp.tile(false);
 		// csg.view(sp.longPlate(3, false));
 		// csg.view(sp.scalableTile(8, 14, false, false, false));
-		csg.view(sp.scalableTile(8, 11, false, true, true));
+		// csg.view(sp.scalableTile(8, 8, false, 0, 0));
 
+		Geometry3D plate = sp.scalableTile(8, 8, false, 0, 2);
+		Geometry3D angle = sp.angleTile(8, 1, 8, 1);
+		csg.view(csg.union3D(plate, angle));
 		// csg.view(tile);
 	}
 
